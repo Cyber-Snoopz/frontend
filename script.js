62b7bc9002a5b5e65a70561de2eec1c8d52ac6a0
Ayush Borage <ayushborage28@gmail.com>
2022-09-24 13:13:23 +0530

    Loading time reduced

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function loader(){
     document.querySelector('.loader-container').classList.add('fade-out')
   }
   function fadeOut(){
-    setInterval(loader, 8000)
+    setInterval(loader, 7000)
   }
   window.onload=fadeOut
